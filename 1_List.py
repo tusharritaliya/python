a5ccaf79b65594742f2d2d6977aa5cddd2b70a24
tusharritaliya <tusharitaliya99@gmail.com>
2026-01-03 11:30:22 +0530

    Add files via upload

---
+++ 1_List.py
@@ -0,0 +1,15 @@
+destination = ["Pune","Mumbai","Delhi","Kashmir","Jaipur"]
+
+Value =["hello",12,12.0,True,12.0,"FF",12,23,23,"Sir"]
+
+
+print(destination + Value)
+print(destination[0])
+print(Value[0])
+print(destination[0:5])
+print(Value[0:5])
+
+print(destination*2)
+print(Value*2)
+
+print(Value[1:6])

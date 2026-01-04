c3c6fcfb676ae4ceb8f778e7e7580d4e9fc0b525
tusharritaliya <tusharitaliya99@gmail.com>
2026-01-04 17:46:07 +0530

    Add files via upload

---
+++ DyInput.py
@@ -0,0 +1,12 @@
+# hello
+
+
+Name = input("Enter Name")
+Branch=input("Enter Branch")
+sem =input("Enter Sem")
+Hob = input("Enter Hobby")
+
+print(Name)
+print(Branch)
+print(sem)
+print(Hob)
---
+++ Variables.py
@@ -0,0 +1,17 @@
+# hello
+
+
+Name = "Tushar"
+address ="panchpipla"
+city ="bhavnagar"
+pincode =364120
+age = 19
+weight = 58
+gender ="male"
+
+print(Name)
+print(address)
+print(pincode)
+print(age)
+print(weight)
+print(gender)

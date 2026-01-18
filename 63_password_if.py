cb5b7a77cf20cc6a2432008d5a8a9aae52e2b11f
Tushar Italiya <tusharitaliya99@gmail.com>
2026-01-18 16:15:18 +0530

    Create 63_password_if.py

---
+++ 63_password_if.py
@@ -0,0 +1,7 @@
+new_password = int(input("Enter new password :"))
+confirm_password =int(input("Enter confirm password :"))
+
+if new_password == confirm_password :
+    print("Confirm password match ")
+else :
+    print("Confirm password does not match ") 

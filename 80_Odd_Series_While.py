433bb659111a15db5ba58433028e3a7355fdcbde
Tushar Italiya <tusharitaliya99@gmail.com>
2026-01-24 13:47:45 +0530

    Create 80_Odd_Series_While.py

---
+++ 80_Odd_Series_While.py
@@ -0,0 +1,9 @@
+
+print("Program To Print Odd number series :")
+print ('______________________________________')
+num = 1
+no = int(input("Enter number : "))
+while num <= no :
+    if num % 2 != 0 :
+        print(f"{num}",end=" ")
+    num =num + 1

37d52a3f2b05d081696492418bac16f71381a20c
Tushar Italiya <tusharitaliya99@gmail.com>
2026-01-25 16:22:39 +0530

    Create 81_Squre_Series_while.py

---
+++ 81_Squre_Series_while.py
@@ -0,0 +1,8 @@
+
+print("Program To Print Square series :")
+print ('______________________________________')
+num = 1
+no = int(input("Enter number : "))
+while num <= no :
+    print(pow(num,2),end=" ")
+    num =num + 1

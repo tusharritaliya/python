c107277e4d5f34d6f13aa78ce91f09b6fca26d1e
Tushar Italiya <tusharitaliya99@gmail.com>
2026-01-25 16:27:45 +0530

    Create 82_Reverce_While.py

---
+++ 82_Reverce_While.py
@@ -0,0 +1,8 @@
+
+print("Program To Print Reverce series :")
+print ('______________________________________')
+
+no = int(input("Enter number : "))
+while no >= 0 :
+    print(no,end=" ")
+    no = no - 5

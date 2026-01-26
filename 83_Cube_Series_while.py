0aa8ce3d75d86d55bedefb3038ba69380d914d0b
Tushar Italiya <tusharitaliya99@gmail.com>
2026-01-26 15:31:58 +0530

    Create 83_Cube_Series_while.py

---
+++ 83_Cube_Series_while.py
@@ -0,0 +1,8 @@
+
+print("Program To Print Cube series :")
+print ('______________________________________')
+num = 1
+no = int(input("Enter number : "))
+while num <= no :
+    print(pow(num,3),end=" ")
+    num = num + 1

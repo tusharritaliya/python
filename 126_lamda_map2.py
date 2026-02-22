ad517d7075914b7144b9972f32761882958d8956
Tushar Italiya <tusharitaliya99@gmail.com>
2026-02-22 18:37:11 +0530

    Add files via upload

---
+++ 126_lamda_map2.py
@@ -0,0 +1,6 @@
+countries = ["india", "uSa", "bRaZiL", "Canada", "aUSTRALIA"]
+
+uppercase_countries = list(map(lambda country: country.upper(), countries))
+
+print("Original List:", countries)
+print("Uppercase List:", uppercase_countries)

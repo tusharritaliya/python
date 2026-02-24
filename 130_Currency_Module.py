c369df33fb0ca58a27626776d69e9918e2d526d7
Tushar Italiya <tusharitaliya99@gmail.com>
2026-02-24 20:29:46 +0530

    Add files via upload

---
+++ 130_Currency_Module.py
@@ -0,0 +1,13 @@
+# currency.py
+
+def toDollar(rupees):
+    return rupees / 83.0
+
+def toEuro(rupees):
+    return rupees / 90.0
+
+def toPound(rupees):
+    return rupees / 105.0
+
+def ToYen(rupees):
+    return rupees / 0.55

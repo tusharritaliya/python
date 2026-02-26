726fb8a7c5b1e918f77f2f0b208330cd8c3839ee
Tushar Italiya <tusharitaliya99@gmail.com>
2026-02-26 20:03:38 +0530

    Add files via upload

---
+++ 132_Module_Weight.py
@@ -0,0 +1,19 @@
+# weight.py
+
+def kg_to_gram(kg):
+    return kg * 1000
+
+def gram_to_kg(gram):
+    return gram / 1000
+
+def kg_to_pound(kg):
+    return kg * 2.20462
+
+def pound_to_kg(pound):
+    return pound / 2.20462
+
+def kg_to_ounce(kg):
+    return kg * 35.274
+
+def ounce_to_kg(ounce):
+    return ounce / 35.274

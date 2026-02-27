664498819a7e691d6354774f04ebee7c51c69693
Tushar Italiya <tusharitaliya99@gmail.com>
2026-02-27 19:26:19 +0530

    Add files via upload

---
+++ 133_measurement_Module.py
@@ -0,0 +1,24 @@
+
+def meter_to_kilometer(meter):
+    return meter / 1000
+
+def kilometer_to_meter(km):
+    return km * 1000
+
+def meter_to_centimeter(meter):
+    return meter * 100
+
+def centimeter_to_meter(cm):
+    return cm / 100
+
+def meter_to_inch(meter):
+    return meter * 39.3701
+
+def inch_to_meter(inch):
+    return inch / 39.3701
+
+def meter_to_foot(meter):
+    return meter * 3.28084
+
+def foot_to_meter(foot):
+    return foot / 3.28084
